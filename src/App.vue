15ff8c63b1b37a59899d51f9213855301adeaa76
vincenzodivita <divitavincenzo@icloud.com>
2023-05-24 17:31:57 +0200

    Import search comp, add function for search in api, add h1 for print first resoult

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script>
+import Search from './components/Search.vue'
+import axios from 'axios';
 import { store } from './store';
 
 export default {
@@ -6,13 +8,37 @@ export default {
 		return {
 			store,
 		};
-	},
+  },
+  components: {
+    Search,
+  },
+  methods: {
+    getMovie() {
+    axios
+        .get(
+          "https://api.themoviedb.org/3/search/movie", {
+            params: {
+              api_key: "e99307154c6dfb0b4750f6603256716d",
+						  query: this.store.searchText,
+						
+					},
+        })
+        .then((response) => (this.store.movieCatalog = response.data.results));
+        console.log(this.store.movieCatalog)
+     },
+  },
+  created() {
+    this.getMovie()
+  }
+
 }
 
 </script>
 
 <template>
- 
+ <Search @performSearch="getMovie"/>
+ <h1>{{ this.store.movieCatalog[0].title }}</h1>
+ <!-- <img src="https://image.tmdb.org/t/p/w342/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg" alt=""> -->
 </template>
 
 <style scoped>
