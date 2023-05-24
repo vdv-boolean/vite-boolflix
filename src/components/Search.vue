75f00ce8418ed062eca6208a4e95077930724176
vincenzodivita <divitavincenzo@icloud.com>
2023-05-24 17:31:11 +0200

    Create search component

---
+++ src/components/Search.vue
@@ -0,0 +1,29 @@
+<script>
+    import { store } from '../store';
+
+    export default {
+        data() {
+            return {
+                store,
+            };
+        },
+        methods: {
+		emitSearchEvent() {
+			this.$emit('performSearch');
+		},
+    },
+}
+</script>
+
+<template>
+    <input
+        type="text"
+        placeholder="Cerca"
+        v-model="store.searchText"
+        @keyup.enter="emitSearchEvent"
+    >
+</template>
+
+<style>
+
+</style>
