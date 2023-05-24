ffb89f26763ea68f13802f0dfa7eb6b1144cdada
vincenzodivita <divitavincenzo@icloud.com>
2023-05-24 18:04:15 +0200

    Add Movie with v-for

---
+++ src/components/MovieList.vue
@@ -0,0 +1,23 @@
+<script>
+    import { store } from '../store';
+
+    export default {
+        data() {
+            return {
+                store,
+            };
+        },
+    }
+</script>
+
+<template>
+    <h1
+        v-for="movie, i in store.movieCatalog"
+    >
+        {{ this.store.movieCatalog[i].title }}
+    </h1>
+</template>
+
+<style>
+
+</style>
