cd37c0b13fa4d9889ea9a21573b97fd3b7f1bb26
vincenzodivita <divitavincenzo@icloud.com>
2023-05-24 18:25:06 +0200

    Add other data

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -11,13 +11,24 @@
 </script>
 
 <template>
-    <h1
-        v-for="movie, i in store.movieCatalog"
-    >
-        {{ this.store.movieCatalog[i].title }}
-    </h1>
+    <div class="result" v-for="movie, i in store.movieCatalog">
+       <div>
+            Titolo: {{ this.store.movieCatalog[i].title }}
+       </div>
+       <div>
+            Titolo originale: {{ this.store.movieCatalog[i].original_title }}
+       </div>
+       <div>
+            Lingua: {{ this.store.movieCatalog[i].original_language }}
+       </div>
+       <div>
+            Voto: {{ this.store.movieCatalog[i].vote_average }}
+       </div>
+    </div>
 </template>
 
 <style>
-
+.result {
+    border: 1px solid black;
+}
 </style>
