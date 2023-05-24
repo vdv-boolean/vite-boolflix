a86d066330bb1cffcf19b22ca3355f8afa29f411
vincenzodivita <divitavincenzo@icloud.com>
2023-05-24 21:03:23 +0200

    Import tv show component and add new functions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import Search from './components/Search.vue'
 import MovieList from './components/MovieList.vue'
+import ShowList from './components/ShowList.vue'
 import axios from 'axios';
 import { store } from './store';
 
@@ -13,6 +14,7 @@ export default {
   components: {
     Search,
     MovieList,
+    ShowList,
   },
   methods: {
     getMovie() {
@@ -28,10 +30,28 @@ export default {
         .then((response) => (this.store.movieCatalog = response.data.results));
         console.log(this.store.movieCatalog)
      },
+     getShow() {
+    axios
+        .get(
+          "https://api.themoviedb.org/3/search/tv", {
+            params: {
+              api_key: "e99307154c6dfb0b4750f6603256716d",
+						  query: this.store.searchText,
+						
+					},
+        })
+        .then((response) => (this.store.showCatalog = response.data.results));
+        console.log(this.store.showCatalog)
+     },
+     getBoth() {
+      this.getMovie();
+      this.getShow();
+     }
   },
   created() {
-    this.getMovie()
-  }
+    this.getMovie();
+    this.getShow();
+  },
 
 }
 
@@ -39,9 +59,10 @@ export default {
 
 <template>
 		
- <Search @performSearch="getMovie()"/>
+ <Search @performSearch="getBoth()"/>
 
  <MovieList />
+ <ShowList />
 
 
  <!-- <img src="https://image.tmdb.org/t/p/w342/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg" alt=""> -->
