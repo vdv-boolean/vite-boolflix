0a7f47e7c355cd23879f04b4858f80345a788713
vincenzodivita <divitavincenzo@icloud.com>
2023-05-26 17:41:05 +0200

    Add draft for result component

---
+++ src/components/Result.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from '../store';
+
+export default {
+    data() {
+        return {
+            store,
+        };
+    },
+}
+</script>
+
+<template>
+        <li>
+            Titolo: {{ this.store.movieCatalog[i].title }}
+       </li>
+       <!-- <li>
+            Titolo originale: {{ this.store.movieCatalog[i].original_title }}
+       </li>
+       <li>
+            Lingua: {{ this.store.movieCatalog[i].original_language }}
+       </li>
+       <li>
+            Voto: {{ this.store.movieCatalog[i].vote_average }}
+       </li>
+       <img :src="this.store.imageLink + this.store.movieCatalog[i].poster_path" alt=""> -->
+</template>
+
+<style>
+
+</style>
