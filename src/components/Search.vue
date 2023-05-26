d107f0ed969e9ae0653a0ca3f029e50db7a7a3bf
vincenzodivita <divitavincenzo@icloud.com>
2023-05-26 17:39:21 +0200

    Add title and flex nav bar items

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -16,17 +16,35 @@
 </script>
 
 <template>
-    <input
-        type="text"
-        placeholder="Cerca"
-        v-model="store.searchText"
-        @keyup.enter="emitSearchEvent"
-    >
-    <button @click="emitSearchEvent">
-        Cerca
-    </button>
+    <nav>
+        <h1>BOOLFLIX</h1>
+        <div>
+            <input
+                type="text"
+                placeholder="Cerca"
+                v-model="store.searchText"
+                @keyup.enter="emitSearchEvent"
+            >
+            <button @click="emitSearchEvent">
+                Cerca
+            </button>
+        </div>
+        
+    </nav>
+    
 </template>
 
-<style>
-
+<style scoped>
+    nav {
+        width: 100%;
+        height: 80px;
+        background-color: black;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding-inline: 1rem;
+    }
+    h1 {
+        color: red;
+    }
 </style>
